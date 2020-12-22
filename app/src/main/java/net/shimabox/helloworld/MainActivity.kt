8ab547c9d3eba2e3ff4886ae07e19c87f8c328e9
shimabox <shimabox@gmail.com>
2020-12-22 21:46:05 +0900

    :sparkles: ボタンをタップした時の処理を記述

--- app/src/main/java/net/shimabox/helloworld/MainActivity.kt
+++ app/src/main/java/net/shimabox/helloworld/MainActivity.kt
@@ -2,10 +2,18 @@ package net.shimabox.helloworld
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import net.shimabox.helloworld.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        binding.tapHere.setOnClickListener {
+            binding.textView.text = "ボタンがタップされました"
+        }
     }
 }
